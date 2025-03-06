66d322f0538ed85e1dc69c526c6db90124f4aa5f
James Morris <jamesmorris2@gmail.com>
2025-03-06 10:48:19 -0500

    Found a better way to get alias table and cleaned up print_aliases()

--- src/cargo-alias.rs
+++ src/cargo-alias.rs
@@ -4,7 +4,7 @@ use anyhow::bail;
 use clap::{Args, Parser};
 use std::fs;
 use std::io;
-use toml_edit::{DocumentMut, Value};
+use toml_edit::{DocumentMut, Table, Value};
 use util::CARGO_HOME;
 
 #[derive(Parser)]
@@ -31,39 +31,39 @@ fn main() -> anyhow::Result<()> {
         },
     };
 
-    if !config.contains_table("alias") {
-        config["alias"] = toml_edit::table();
-    }
+    let aliases = config
+        .entry("alias")
+        .or_insert_with(toml_edit::table)
+        .as_table_mut()
+        .unwrap();
 
     if let Some(new_alias) = opt.alias {
         let (alias, commands) = new_alias.split_once('=').unwrap();
-        config["alias"][&alias] = toml_edit::value(commands);
+        aliases[alias] = toml_edit::value(commands);
         fs::write(CARGO_HOME.as_path(), config.to_string())?;
     } else {
-        print_aliases(config)?;
+        print_aliases(aliases)?;
     }
 
     Ok(())
 }
 
-fn print_aliases(config: DocumentMut) -> anyhow::Result<()> {
+fn print_aliases(aliases: &Table) -> anyhow::Result<()> {
     // None of the unwraps here should ever fail because
     // cargo will validate the config and complain
     // before we even get to run.
-    for alias in config["alias"].as_table().unwrap().iter() {
-        let (alias_name, val) = alias;
-
-        let val = match val.as_value().unwrap() {
-            Value::String(s) => s.value().into(),
-            Value::Array(a) => a
+    for (name, value) in aliases {
+        let value = match value.as_value().unwrap() {
+            Value::String(s) => s.value(),
+            Value::Array(a) => &a
                 .iter()
                 .map(|i| i.as_str().unwrap())
                 .collect::<Vec<&str>>()
                 .join(" "),
-            _ => bail!("value of {} is not a list or string", alias_name),
+            _ => bail!("value of {name} is not a list or string"),
         };
 
-        println!("cargo alias {}='{}'", alias_name, val);
+        println!("cargo alias {name}='{value}'");
     }
 
     Ok(())
